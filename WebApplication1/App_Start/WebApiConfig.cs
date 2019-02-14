398ef4d1e2120b16a752c162b1224ce6e9d0a403
Ngwe Soe Lin <ngwesoelin@onthego.com.sg>
2019-02-14 15:31:00 +0630

    Version_1

---
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            //GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer = true;
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+            //.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer = true;
+
+
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomerController : ApiController
+    {
+        DataTable dt = new DataTable();
+        public string GetTest()
+        {
+            return "asdf";
+        }
+
+        
+        public DataTable GetTable()
+        {
+            dt.TableName = "test";
+            dt.Columns.Add("ID", typeof(String));
+            dt.Columns.Add("Name", typeof(String));
+
+            DataRow dr = dt.NewRow();
+            dt.Rows.Add(new Object[]{"1", "Name1"});
+
+            return dt;
+        }
+    }
+}
